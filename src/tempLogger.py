2a848254fd410f44fb7395674f419f39db1a8384
Curtis Sapp <curtmsapp@gmail.com>
2020-08-06 14:11:27 -0700

    Added settings class

--- src/tempLogger.py
+++ src/tempLogger.py
@@ -8,28 +8,35 @@ from datetime import datetime
 def isLinux():
     return platform.system() == "Linux"
 
+
 def debugSensorData():
     return -1, -1
 
-def start():
-    # Debug settings
-    getSensorData = debugSensorData
-    readingPeriod = 5 #  Seconds
-    FILE_PATH = "tempReadings.log"
-    if isLinux():
-        # Production settings
-        from .tempUtil import getCurrentTempAndHummidity
-        getSensorData = getCurrentTempAndHummidity
-        readingPeriod = 60 # Seconds
-        FILE_PATH = "/home/pi/code/tempReadings.log"
 
+class Settings:
+    def __init__(self):
+        if (isLinux()):
+            # Production settings
+            from .tempUtil import getCurrentTempAndHummidity
+            self.getSensorData = getCurrentTempAndHummidity
+            self.readingPeriod = 60  # Seconds
+            self.FILE_PATH = "/home/pi/code/tempReadings.log"
+        else:
+            # Debug settings
+            self.getSensorData = debugSensorData
+            self.readingPeriod = 5  # Seconds
+            self.FILE_PATH = "./tempReadings.log"
+
+
+def start():
+    settings = Settings()
     oldtime = time.time()
     while True:
-        if time.time() - oldtime > readingPeriod:
-            temp, humidity = getSensorData()
+        if time.time() - oldtime > settings.readingPeriod:
+            temp, humidity = settings.getSensorData()
             dt_string = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
             if humidity is not None and temp is not None:
-                with open(FILE_PATH, "a") as myfile:
+                with open(settings.FILE_PATH, "a") as myfile:
                     myfile.write('{0},{1:0.0f},{2:0.0f}\n'.format(dt_string, temp, humidity))
             else:
                 myfile.write('Failed to get reading. Try again!\n')
